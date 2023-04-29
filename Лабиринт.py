13e7638417f9b7ffdbb19255ebc6cb8c90d5c340
artkotenko <132163249+artkotenko@users.noreply.github.com>
2023-04-29 22:18:11 +0300

    Update Лабиринт.py

--- Лабиринт.py
+++ Лабиринт.py
@@ -6,7 +6,7 @@ class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
 
-        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.image = transform.scale(image.load(player_image), (60, 60))
         self.speed = player_speed
 
         self.rect = self.image.get_rect()
